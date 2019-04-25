e6748099644b308660536fbd955f68216e7f09e0
Mr.zhao <zhaozonghu@yonrun.com>
2019-04-25 16:40:39 +0800

    update

---
+++ 花瓣飘落/js/flowerfall.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.valent_wrap').snowfall({image:"images/1.png", flakeCount:1,minSpeed:2, maxSpeed:4, minSize:36, maxSize:60,});
+    $('.valent_wrap').snowfall({image:"images/3.png", flakeCount:1,minSpeed:2, maxSpeed:4, minSize:28, maxSize:50,});
+    $('.valent_wrap').snowfall({image:"images/2.png", flakeCount:1,minSpeed:2, maxSpeed:4, minSize:28, maxSize:50,});
+    $('.valent_wrap').snowfall({image:"images/2.png", flakeCount:1,minSpeed:2, maxSpeed:4, minSize:28, maxSize:50,});
+    $('.valent_wrap').snowfall({image:"images/4.png", flakeCount:1,minSpeed:2, maxSpeed:4, minSize:28, maxSize:50,});
+});
